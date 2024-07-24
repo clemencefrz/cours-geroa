2ab12d7d5734389de620a6fbcc4ad7468f8d3168
Clémence <clemencefrz@gmail.com>
2024-07-24 09:49:30 +0200

    barre de dnav

--- app/components/Header/MobileNav.tsx
+++ app/components/Header/MobileNav.tsx
@@ -1,7 +1,7 @@
 import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
 import Logo from "../Logo";
 import Socials from "../Socials";
-import Nav from "./Nav";
+import { Nav } from "./Nav";
 
 const MobileNav = () => {
   return (
@@ -11,14 +11,8 @@ const MobileNav = () => {
       </SheetTrigger>
       <SheetContent>
         <div className="flex flex-col items-center justify-between h-full py-8">
-          <div className="flex flex-col items-center gap-y-32">
-            <Logo />
-            <Nav
-              containerStyles={"flex flex-col items-center gap-y-6"}
-              linkStyles={"text-2xl"}
-              underlineStyles={""}
-            />
-          </div>
+          <Logo />
+          <Nav />
           <Socials containerStyles={"flex gap-x-4"} iconStyles={"text-2xl"} />
         </div>
       </SheetContent>
--- app/components/Header/index.tsx
+++ app/components/Header/index.tsx
@@ -1,29 +1,21 @@
 "use client";
 
 import Logo from "../Logo";
-import Nav from "./Nav";
+
 import MobileNav from "./MobileNav";
+import { Nav } from "./Nav";
 
 const Header = () => {
   return (
     <header>
       <div className="container mx-auto">
-        <div className="flex justify-between items-center">
+        <div className="flex">
           <Logo />
-
-          <div className="flex items-center gap-x-6">
-            {/* nav */}
-            <Nav
-              containerStyles={"hidden xl:flex gap-x-8 items-center"}
-              linkStyles={"relative hover:text-primary transition-all"}
-              underlineStyles={
-                "absolute left-0 top-full h-[2px] bg-primary w-full"
-              }
-            />
-            {/* mobile nav */}
-            <div className="xl:hidden">
-              <MobileNav />
-            </div>
+          {/* nav */}
+          <Nav />
+          {/* mobile nav */}
+          <div className="xl:hidden">
+            <MobileNav />
           </div>
         </div>
       </div>
