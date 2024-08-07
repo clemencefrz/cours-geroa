ab263328c52e4a0ccea797ad6d89aa9d0bc083d4
Clémence <clemencefrz@gmail.com>
2024-08-07 17:34:31 +0200

    improve homepage

--- app/components/Home.tsx
+++ app/components/Home.tsx
@@ -1,33 +1,11 @@
 "use client";
-import { HeroHighlight, Highlight } from "@/components/ui/hero-highlight";
-import { motion } from "framer-motion";
+
+import { TitleHome } from "./TitleHome";
 
 const Home = () => {
   return (
     <div>
-      <HeroHighlight>
-        <motion.h1
-          initial={{
-            opacity: 0,
-            y: 20,
-          }}
-          animate={{
-            opacity: 1,
-            y: [20, -5, 0],
-          }}
-          transition={{
-            duration: 0.5,
-            ease: [0.4, 0.0, 0.2, 1],
-          }}
-          className="text-2xl px-4 md:text-4xl lg:text-5xl font-bold text-neutral-700 dark:text-white max-w-4xl leading-relaxed lg:leading-snug text-center mx-auto "
-        >
-          {`Réussir des études et trouver sa voix On fait des mathématiques, de la
-            physique, de l'info`}
-          <Highlight className="text-black dark:text-white">
-            petit à petit.
-          </Highlight>
-        </motion.h1>
-      </HeroHighlight>
+      <TitleHome />
       <div>Pourquoi on résout tes problèmes</div>
       <div>Découvre les profs, découvre nos cours</div>
     </div>
--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,10 @@
+import { Metadata } from "next";
 import Home from "./components/Home";
+export const metadata: Metadata = {
+  title: "Cours Geroa - Soutien scolaire - Réussite en Maths, Physique et Info",
+  description:
+    "Découvrez notre soutien scolaire sur mesure pour lycéens et étudiants. Améliorez vos notes, gagnez en confiance et préparez votre avenir avec des cours en maths, physique et informatique adaptés à vos besoins.",
+};
 
 export default function Page() {
   return <Home />;
