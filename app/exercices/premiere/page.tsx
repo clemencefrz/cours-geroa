de1a25a43507d5fec72c83c2bd2d67d893dd416b
Clemence <clemencefrz@gmail.com>
2024-12-23 15:15:44 +0100

    fix style page premiere

--- app/exercices/premiere/page.tsx
+++ app/exercices/premiere/page.tsx
@@ -4,7 +4,7 @@ import ChapterCard from "@/app/_components/specific/exercices/chapter-card";
 const Page = () => {
   const chapters = data.chapters.filter((chapter) => chapter.levelId === 2);
   return (
-    <div className="ContainerHeaderPage">
+    <div className="flex flex-col h-full items-center gap-4">
       <h1>Chapitres </h1>
       {chapters.map((chapter, key) => (
         <ChapterCard key={key} chapter={chapter} level={{ name: "Première" }} />
