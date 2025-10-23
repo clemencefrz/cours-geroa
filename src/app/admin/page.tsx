3e55f225c60a74949f3b81f66e422af143887a1e
Clémence Fernandez <clemencefrz@gmail.com>
2025-10-23 17:42:17 +0200

    Créer la page admin, accessible qu'en local.

---
+++ src/app/admin/page.tsx
@@ -0,0 +1,11 @@
+
+export default function AdminPage() {
+    return (
+        <div className="container mx-auto px-4 py-8">
+            <h1 className="text-4xl font-bold mb-8">
+                Tableau de bord - Admin
+            </h1>
+        </div>
+    );
+}
+
