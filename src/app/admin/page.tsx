b381961f455d2258a690e3d132ce8e0f03486bea
Clémence Fernandez <clemencefrz@gmail.com>
2025-10-23 17:55:31 +0200

    Créer un tableau de bord

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,3 +1,6 @@
+import { DataTable } from "@/components/ui/data-table"
+import { columns } from "@/components/specific/admin/columns"
+import sujetsData from "@/../public/sujets.json"
 
 export default function AdminPage() {
     return (
@@ -5,6 +8,13 @@ export default function AdminPage() {
             <h1 className="text-4xl font-bold mb-8">
                 Tableau de bord - Admin
             </h1>
+            <div className="mt-8">
+                <h2 className="text-2xl font-semibold mb-4">Sujets de Bac</h2>
+                <DataTable
+                    columns={columns}
+                    data={sujetsData}
+                />
+            </div>
         </div>
     );
 }
